7734c318f8524245a98d1381d3916d1ccddee8b7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-22 01:18:25 -0700

    chore: move openAI service to separate service folder

---
+++ CareerKitBackend/Main/AIService/Exceptions/ChatRequestErrorException.cs
@@ -0,0 +1,9 @@
+﻿namespace CareerKitBackend.Main.AIService.Exceptions
+{
+	public class ChatRequestErrorException : Exception
+	{
+		public ChatRequestErrorException() { }
+		public ChatRequestErrorException(string message) : base(message) { }
+		public ChatRequestErrorException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
---
+++ CareerKitBackend/Main/AIService/Exceptions/EmptyResponseException.cs
@@ -0,0 +1,9 @@
+﻿namespace CareerKitBackend.Main.AIService.Exceptions
+{
+	public class EmptyResponseException : Exception
+	{
+		public EmptyResponseException() : base() { }
+		public EmptyResponseException(string message) : base(message) { }
+		public EmptyResponseException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
