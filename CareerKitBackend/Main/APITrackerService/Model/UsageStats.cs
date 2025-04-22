f94d5fb8437a333a52f4ae03c1e1983ea43f9a37
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-21 21:19:19 -0700

    feat: create tracker service

---
+++ CareerKitBackend/Main/APITrackerService/Model/UsageStats.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+
+namespace CareerKitBackend.Main.APITrackerService.Model
+{
+	public record class UsageStats
+	{
+		public int CoverLetterAutoFillRemaining { get; private set; } = 20;
+
+		public void Decrement(ServiceEndpointsEnum service)
+		{
+			switch(service)
+			{
+				case ServiceEndpointsEnum.CoverLetterAutofillService:
+					CoverLetterAutoFillRemaining--;
+					break;
+			}
+		}
+	}
+}
---
+++ CareerKitBackend/Main/APITrackerService/Service/TrackerService.cs
@@ -0,0 +1,26 @@
+﻿using CareerKitBackend.Main.APITrackerService.Model;
+
+namespace CareerKitBackend.Main.APITrackerService.Service
+{
+	public class TrackerService
+	{
+		private readonly Dictionary<string, UsageStats> ipUsage = [];
+
+		public void DecrementUsage(ServiceEndpointsEnum service, string IPAddress)
+		{			
+			if (!ipUsage.ContainsKey(IPAddress))
+			{
+				UsageStats newStats = new();
+				newStats.Decrement(service);
+				ipUsage.Add(IPAddress, newStats);
+				return;
+			}
+			ipUsage[IPAddress].Decrement(service);
+		}
+
+		public void ResetUsages()
+		{
+			ipUsage.Clear();
+		}
+	}
+}
