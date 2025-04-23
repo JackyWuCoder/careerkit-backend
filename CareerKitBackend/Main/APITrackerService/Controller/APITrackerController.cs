92e77b8743a487ee98efd374fc93c0c14a5b7bd8
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-22 17:07:04 -0700

    feat: implement endpoint for getting usage stats

---
+++ CareerKitBackend/Main/APITrackerService/Controller/APITrackerController.cs
@@ -0,0 +1,21 @@
+﻿using CareerKitBackend.Main.APITrackerService.DTO;
+using CareerKitBackend.Main.APITrackerService.Model;
+using CareerKitBackend.Main.APITrackerService.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerKitBackend.Main.APITrackerService.Controller
+{
+	[Route("api/v1/[controller]")]
+	[ApiController]
+	public class APITrackerController(TrackerService trackerService) : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult GetAPIStats()
+		{
+			// TODO: Implement getting IP address from requester
+			string requestIPAddress = "fillerIPHere";
+			UsageStats stats = trackerService.GetUsageStat(requestIPAddress);
+			return Ok(new GetUsageStatsResponse(stats));
+		}
+	}
+}
--- CareerKitBackend/Main/APITrackerService/Service/TrackerService.cs
+++ CareerKitBackend/Main/APITrackerService/Service/TrackerService.cs
@@ -8,32 +8,49 @@ namespace CareerKitBackend.Main.APITrackerService.Service
 		private readonly Dictionary<string, UsageStats> _ipUsage = [];
 
 		public void DecrementUsage(ServiceEndpointsEnum service, string IPAddress)
-		{			
-			if (!_ipUsage.ContainsKey(IPAddress))
-			{
-				UsageStats newStats = new();
-				newStats.Decrement(service);
-				_ipUsage.Add(IPAddress, newStats);
-				return;
-			}
+		{
+			if (!_ipUsage.ContainsKey(IPAddress)) AddNewIPStats(IPAddress, service);
 			_ipUsage[IPAddress].Decrement(service);
+		}
+
+		public UsageStats GetUsageStat(string IPAddress)
+		{
+			if(!_ipUsage.TryGetValue(IPAddress, out UsageStats? stats)) return AddNewIPStats(IPAddress);
+			return stats;
 		}
 
 		public int GetRemainingUse(ServiceEndpointsEnum service, string IPAddress)
 		{
-			if (!_ipUsage.TryGetValue(IPAddress, out UsageStats? value)) return UsageStats.StartingUseCount;
+			if (!_ipUsage.TryGetValue(IPAddress, out UsageStats? value))
+			{
+				UsageStats stats = AddNewIPStats(IPAddress);
+				return stats.GetRemainingUsage(service);
+			}
 			return value.GetRemainingUsage(service);
 		}
 
 		public bool CanUseService(ServiceEndpointsEnum service, string IPAddress)
 		{
-			if (!_ipUsage.TryGetValue(IPAddress, out UsageStats? value)) return true; // New IPs will create UsageStats upon DecrementUsage invocation
+			if (!_ipUsage.TryGetValue(IPAddress, out UsageStats? value))
+			{
+				AddNewIPStats(IPAddress);
+				return true;
+			}
 			return value.GetRemainingUsage(service) > 0;
 		}
 
 		public void ResetUsages()
 		{
 			_ipUsage.Clear();
+		}
+
+		// Adds a new IP to the dictionary, will automatically decrement if a service is provided from which it was called from
+		private UsageStats AddNewIPStats(string IPAddress, ServiceEndpointsEnum? service = null)
+		{
+			UsageStats newStats = new();
+			_ipUsage.Add(IPAddress, newStats);
+			if (service != null) newStats.Decrement((ServiceEndpointsEnum) service);			
+			return newStats;
 		}
 	}
 }
