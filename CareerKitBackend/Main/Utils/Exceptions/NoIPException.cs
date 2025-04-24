64a15ae10d2393ac55231a90e0697cf83fcfa1f7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-23 23:17:04 -0700

    feat: create exception if unable to get IP from client

---
+++ CareerKitBackend/Main/Utils/Exceptions/NoIPException.cs
@@ -0,0 +1,9 @@
+﻿namespace CareerKitBackend.Main.Utils.Exceptions
+{
+	public class NoIPException : Exception
+	{
+		public NoIPException() { }
+		public NoIPException(string message) : base(message) { }
+		public NoIPException(string message,  Exception innerException) : base(message, innerException) { }
+	}
+}
