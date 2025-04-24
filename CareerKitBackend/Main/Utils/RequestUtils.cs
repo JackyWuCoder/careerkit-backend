3933c1a4540bce8410fbd8863ad1994cfcd531f7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-23 23:28:38 -0700

    fix: remove unnecessary check for localhost ip

--- CareerKitBackend/Main/Utils/RequestUtils.cs
+++ CareerKitBackend/Main/Utils/RequestUtils.cs
@@ -6,12 +6,11 @@ namespace CareerKitBackend.Main.Utils
 	{
 		public static string GetIPAddress(HttpContext context)
 		{
-			string? ip = context.Connection.RemoteIpAddress?.ToString();
-			if (context.Request.Headers.ContainsKey("X-Forwarded-For"))
-				ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+			string? ip = context.Request.Headers.ContainsKey("X-Forwarded-For") ? 
+				context.Request.Headers["X-Forwarded-For"].FirstOrDefault() : 
+				context.Connection.RemoteIpAddress?.ToString();
 
 			if (string.IsNullOrEmpty(ip)) throw new NoIPException("Unable to identify user");
-			else if (ip == "::1") return "127.0.0.1"; // Shows up as ::1 if using localhost
 			return ip;
 		}
 	}
