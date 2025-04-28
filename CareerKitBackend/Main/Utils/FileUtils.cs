0ed368a22dffaf3e88c406341936b957c042177b
Jacky Wu <JackyWuCoder@gmail.com>
2025-04-28 15:50:19 -0700

    feat: file parser utility class for reading a file

---
+++ CareerKitBackend/Main/Utils/FileUtils.cs
@@ -0,0 +1,16 @@
+namespace CareerKitBackend.Main.Utils;
+
+public static class FileParser
+{
+    private static readonly string BasePath = Path.Combine(AppContext.BaseDirectory, "Resources");
+
+    public static string ReadFile(string fileName)
+    {
+        string filePath = Path.Combine(BasePath, fileName);
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"File not found: {filePath}");
+        }
+        return File.ReadAllText(filePath);
+    }
+}
