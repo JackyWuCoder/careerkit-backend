ba55785bae7cd1422ba5e2211852e39942723627
Jacky Wu <JackyWuCoder@gmail.com>
2025-04-28 16:04:52 -0700

    feat: add the replacement marker inside the FileParser class

--- CareerKitBackend/Main/Utils/FileUtils.cs
+++ CareerKitBackend/Main/Utils/FileUtils.cs
@@ -2,6 +2,7 @@ namespace CareerKitBackend.Main.Utils;
 
 public static class FileParser
 {
+    public static readonly string Marker = "[%R%]";
     private static readonly string BasePath = Path.Combine(AppContext.BaseDirectory, "Resources");
 
     public static string ReadFile(string fileName)
