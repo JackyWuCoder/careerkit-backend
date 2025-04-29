755f21c4d78a8cf15490242afce3418d2e8ee2a7
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-04-28 19:09:00 -0700

    feat: Endpoint For Cover Letter Proofreading/Advice (#7)
    * feat: create util class to help extract string sections from ai response
    
    * refactor: remove required from response DTO
    
    * feat: create new endpoint for proofreading and getting cover letter advice
    
    * feat: add best practices for cover letter criterion

---
+++ CareerKitBackend/Main/Utils/StringParseUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace CareerKitBackend.Main.Utils
+{
+	public static class StringParseUtils
+	{
+		public static string ExtractSection(string content, string sectionName)
+		{
+			string delimiter = $"[---{sectionName}---]";
+			int indexFrom = content.IndexOf(delimiter) + delimiter.Length;
+			int indexTo = content.LastIndexOf(delimiter);
+			return content[indexFrom..indexTo].Trim();
+		}
+	}
+}
