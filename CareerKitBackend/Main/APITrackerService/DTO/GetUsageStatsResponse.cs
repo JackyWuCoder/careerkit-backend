32c07cc37f3a051e80594e2795b1cf5b49f9872f
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2025-04-30 06:06:47 -0700

    feat: create new service for generating interview questions

--- CareerKitBackend/Main/APITrackerService/DTO/GetUsageStatsResponse.cs
+++ CareerKitBackend/Main/APITrackerService/DTO/GetUsageStatsResponse.cs
@@ -7,6 +7,7 @@ namespace CareerKitBackend.Main.APITrackerService.DTO
 		public int CoverLetterAutofill { get; set; } = stats.CoverLetterAutoFillRemaining;
 		public int CoverLetterFromScratch { get; set; } = stats.CoverLetterScratchService;
 		public int CoverLetterProofread { get; set; } = stats.CoverLetterProofreadService;
+		public int InterviewQuestions { get; set; } = stats.InterviewQuestions;
 		public int ResumeAutofill { get; set; } = stats.ResumeAutoFillRemaining;
 		public int AdviceService { get; set; } = stats.AdviceServiceRemaining;
 	}
